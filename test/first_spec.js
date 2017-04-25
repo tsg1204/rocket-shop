0599bca2d394a645da10f9b809c577c05495d005
Tatiana <tsg1204@gmail.com>
2017-04-25 08:49:11 -0400

    Initial load

---
+++ test/first_spec.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+
+describe("A feature", function() {
+	describe("A scenario", function() {
+		it("does something expected", function() {
+			assert(true);
+			console.log("Success!")
+		})
+	})
+})
